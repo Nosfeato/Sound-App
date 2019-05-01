e111133e1a084f894fbc673ce401568c574de06d
Nosfeato <boitaordures@hotmail.com>
2019-05-01 17:48:22 +0200

    fixed wrong png file for Responsive testing

--- src/components/App/Playbacks/Track/index.js
+++ src/components/App/Playbacks/Track/index.js
@@ -97,7 +97,20 @@ class Track extends React.Component {
             </ul>
             <div className="track__volume">
               <span className="track__volume__identifier">Volume :</span>
-              <input type="range" min="0" max="100" name="volume" className="track__volume__input" onChange={this.changeTrackVolume} />
+              <input type="range" min="0" max="100" name="volume" list="tickmarks" className="track__volume__input" onChange={this.changeTrackVolume} />
+              <datalist id="tickmarks">
+                <option value="0" label="0%" />
+                <option value="10" />
+                <option value="20" />
+                <option value="30" />
+                <option value="40" />
+                <option value="50" label="50%" />
+                <option value="60" />
+                <option value="70" />
+                <option value="80" />
+                <option value="90" />
+                <option value="100" label="100%" />
+              </datalist>
             </div>
           </div>
         </div>
